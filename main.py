668bc4eefeafeb429ffcf84b00327509e9dafef6
pchai <prempreychhay@gmail.com>
2025-03-09 08:10:11 -0700

    it's moving!

--- main.py
+++ main.py
@@ -12,101 +12,23 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.player = Player((400, 300), self.all_sprites)
 
-        self.front_idle = [
-            self.player.player_image.get_image(0, 0, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(1, 0, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(2, 0, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(3, 0, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(4, 0, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(5, 0, 48, 48, 3, BLACK),
-        ]
-        
-        self.side_idle = [
-            self.player.player_image.get_image(0, 1, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(1, 1, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(2, 1, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(3, 1, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(4, 1, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(5, 1, 48, 48, 3, BLACK),
-        ]
-        
-        self.back_idle = [
-            self.player.player_image.get_image(0, 2, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(1, 2, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(2, 2, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(3, 2, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(4, 2, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(5, 2, 48, 48, 3, BLACK),
-        ]
-        
-        self.front_walk = [
-            self.player.player_image.get_image(0, 3, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(1, 3, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(2, 3, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(3, 3, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(4, 3, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(5, 3, 48, 48, 3, BLACK),
-        ]
-        
-        self.side_walk = [
-            self.player.player_image.get_image(0, 4, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(1, 4, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(2, 4, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(3, 4, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(4, 4, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(5, 4, 48, 48, 3, BLACK),
-        ]
-        
-        self.back_walk = [
-            self.player.player_image.get_image(0, 5, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(1, 5, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(2, 5, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(3, 5, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(4, 5, 48, 48, 3, BLACK),
-            self.player.player_image.get_image(5, 5, 48, 48, 3, BLACK),
-        ]
-        
-        self.side_idle_left = [pygame.transform.flip(frame, True, False) for frame in self.side_idle]
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.all_sprites.update()  # Player and other sprites update
+            self.draw()
+            self.clock.tick(FRAMERATE)
 
-        self.current_animation = self.front_idle
-        self.current_frame = 0
-        self.animation_speed = 150
-        self.last_update = pygame.time.get_ticks()
+        pygame.quit()              
         
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-                
-            elif event.type == pygame.KEYDOWN:
-                if event.key in (pygame.K_RIGHT, pygame.K_d):
-                    self.current_animation = self.side_idle
-                elif event.key in (pygame.K_DOWN, pygame.K_s):
-                    self.current_animation = self.front_idle
-                elif event.key in (pygame.K_LEFT, pygame.K_a):
-                    self.current_animation = self.side_idle_left
-                elif event.key in (pygame.K_UP, pygame.K_w):
-                    self.current_animation = self.back_idle
-                    
-    def update_animation(self):
-        now = pygame.time.get_ticks()
-        if now - self.last_update >= self.animation_speed:
-            self.last_update = now
-            self.current_frame = (self.current_frame + 1) % len(self.current_animation)
-
-    def run(self):
-        while self.running:
-            self.handle_events()
-            self.update_animation()
-            self.all_sprites.update()
-            self.draw()
-            self.clock.tick(FRAMERATE)
-
-        pygame.quit()
 
     def draw(self):
         self.display_surface.fill(BG_COLOR)
-        self.display_surface.blit(self.current_animation[self.current_frame], (200, 200))
+        self.all_sprites.draw(self.display_surface)  # Draw all sprites
         pygame.display.update()
 
 if __name__ == '__main__':
--- sprites.py
+++ sprites.py
@@ -5,7 +5,6 @@ class SpriteSheet():
         self.sheet = image
 
     def get_image(self, frame, row, width, height, scale, colour):
-        """ Extracts an image from the sprite sheet. """
         image = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
         
         x = frame * width
@@ -21,6 +20,65 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load(join('characters', 'player.png')).convert_alpha()
-        self.rect = self.image.get_frect(center = pos)
+        self.rect = self.image.get_frect(center=pos)
         self.player_image = SpriteSheet(self.image)
-        
+
+        self.direction = pygame.Vector2(0, 0)
+        self.speed = 2
+
+        # Animations
+        self.front_idle = [self.player_image.get_image(i, 0, 48, 48, 3, BLACK) for i in range(6)]
+        self.side_idle = [self.player_image.get_image(i, 1, 48, 48, 3, BLACK) for i in range(6)]
+        self.back_idle = [self.player_image.get_image(i, 2, 48, 48, 3, BLACK) for i in range(6)]
+        self.front_walk = [self.player_image.get_image(i, 3, 48, 48, 3, BLACK) for i in range(6)]
+        self.side_walk = [self.player_image.get_image(i, 4, 48, 48, 3, BLACK) for i in range(6)]
+        self.back_walk = [self.player_image.get_image(i, 5, 48, 48, 3, BLACK) for i in range(6)]
+
+        self.side_idle_left = [pygame.transform.flip(frame, True, False) for frame in self.side_idle]
+        self.side_walk_left = [pygame.transform.flip(frame, True, False) for frame in self.side_walk]
+
+        self.current_animation = self.front_idle
+        self.current_frame = 0
+        self.animation_speed = 150
+        self.last_update = pygame.time.get_ticks()
+
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        self.direction.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+
+    def move(self):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
+
+    def update_animation(self):
+        # Determine the correct animation based on movement
+        if self.direction.x > 0:
+            self.current_animation = self.side_walk
+        elif self.direction.x < 0:
+            self.current_animation = self.side_walk_left
+        elif self.direction.y > 0:
+            self.current_animation = self.front_walk
+        elif self.direction.y < 0:
+            self.current_animation = self.back_walk
+        else:
+            if self.current_animation in (self.side_walk, self.side_walk_left):
+                self.current_animation = self.side_idle if self.current_animation == self.side_walk else self.side_idle_left
+            elif self.current_animation == self.front_walk:
+                self.current_animation = self.front_idle
+            elif self.current_animation == self.back_walk:
+                self.current_animation = self.back_idle
+
+        # Frame update logic
+        now = pygame.time.get_ticks()
+        if now - self.last_update >= self.animation_speed:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(self.current_animation)
+            self.image = self.current_animation[self.current_frame]  # Update the player's image
+
+    def update(self):
+        self.handle_input()
+        self.move()
+        self.update_animation()
